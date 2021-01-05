5f4286b92fd394469ee978b67d0e19122fcc63a4
Milena Grncarova <milenagrncarov@outlook.com>
2021-01-05 18:04:04 +0100

    added category

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,10 +2,10 @@ class Listing < ApplicationRecord
     belongs_to :user
     has_many :users, through: :bookings
     has_many :bookings
-    has_many_attached :photos
+    # has_many_attached :photos
 
     SIZE = ["S", "M", "L", "XL", "ХХL"]
-    CATEGORY  ["Фармерки", "Панталони","Кратки Панталнои","Хеланки","Сукња","Кошула","Топ","Палто","Фустан","Блуза","Сако","Комплет","Тренерки"]
+    CATEGORY = ["Фармерки", "Панталони","Кратки Панталнои","Хеланки","Сукња","Кошула","Топ","Палто","Фустан","Блуза","Сако","Комплет","Тренерки"]
     validates :title, presence: true
     validates :category,presence: true
     validates :price, presence: true
