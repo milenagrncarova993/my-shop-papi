80f5580796994cd72ad6de75b6218c86ce902b8f
Milena Grncarova <milenagrncarov@outlook.com>
2021-01-05 18:02:22 +0100

    Added options for size and category

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,18 @@
 class Listing < ApplicationRecord
+    belongs_to :user
+    has_many :users, through: :bookings
+    has_many :bookings
+    has_many_attached :photos
+
+    SIZE = ["S", "M", "L", "XL", "ХХL"]
+    CATEGORY  ["Фармерки", "Панталони","Кратки Панталнои","Хеланки","Сукња","Кошула","Топ","Палто","Фустан","Блуза","Сако","Комплет","Тренерки"]
+    validates :title, presence: true
+    validates :category,presence: true
+    validates :price, presence: true
+    validates :size, presence: true
+    validates :description, presence: true
+    validates :size, inclusion: { in: SIZE, message: "Невалидна категорија" }
+    validates :category, inclusion: { in: CATEGORY, message: "Невалидна категорија" }
+
+
 end
