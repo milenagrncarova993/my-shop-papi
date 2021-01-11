d749339e29a6508c8069149371b92a1385c8717f
Milena Grncarova <milenagrncarov@outlook.com>
2021-01-11 12:59:54 +0100

    Create method on listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,6 +3,20 @@ class ListingsController < ApplicationController
   def new
     @listing = Listing.new
   end
+  
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+    redirect_to @lisitng
+    else
+    render :new
+  end
+  end
+
+  private
     
+  def listing_params
+    params.require(:listing).permit(:title, :category, :price, :size, :description)
+  end
     
 end
